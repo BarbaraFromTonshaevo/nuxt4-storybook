0dfff540e830a2228bc5f05d39f3e369feeaae23
Elkina Barbara <elkinavarvara1999@gmail.com>
2026-02-25 16:06:43 +0300

    fix [nuxt.config]: add autoprefixer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image', '@pinia/nuxt']
+  modules: ['@nuxt/eslint', '@nuxt/image', '@pinia/nuxt'],
+  postcss: {
+    plugins: {
+      autoprefixer: {},
+    }
+  }
 })
