18edf7462f86c991e353222a39c737e4f856d649
Elkina Varvara <elkinavarvara1999@gmail.com>
2026-02-26 17:00:11 +0300

    fix [config]: add styles and fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,34 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image', '@pinia/nuxt'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@pinia/nuxt',
+    '@nuxt/fonts'
+  ],
   postcss: {
     plugins: {
       autoprefixer: {},
     }
+  },
+  css: ['./app/assets/styles/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "./app/assets/styles/abstracts/breakpoints" as *; @use "./app/assets/styles/abstracts/mixins" as *;'
+        }
+      }
+    }
+  },
+  fonts: {
+    families: [
+      {
+        name: 'TikTok Sans',
+        provider: 'google',
+        weights: ['400', '500', '600', '700']
+      }
+    ]
   }
 })
